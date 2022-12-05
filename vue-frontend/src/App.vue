4f9cf67d0b7b8bfbe7105d72dec920baeb1fbaf1
GuilletJoseph <guilletjoseph85@gmail.com>
2022-12-05 11:22:22 +0100

    frontend projet movies

---
+++ vue-frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Films />
+  </div>
+</template>
+
+<script>
+import Films from './components/Films.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Films
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-frontend/src/components/Films.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class = "container">
+            
+            <h1 class = "text-center"> Films List</h1>
+
+            <table class = "table table-striped">
+                <thead>
+                    <tr>
+                        <th> Film Id</th>
+                        <th> Film Id imdb</th>
+                        <th> Film Nom</th>
+                        <th> Film Url</th>
+                    </tr>
+
+                </thead>
+                <tbody>
+                    <tr v-for="film in films" v-bind:key="film.idImdb">
+                        <td> {{film.id }}</td>
+                        <td> {{film.idImdb }}</td>
+                        <td> {{film.nom}}</td>    
+                        <td> {{film.url}}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+</template>
+
+<script>
+
+import FilmService from '../services/FilmService';
+
+export default {
+    name: 'Films_',
+    data(){
+        return {
+            films: []
+        }
+    },
+    methods: {
+        getFilms(){
+            FilmService.getFilms().then((response) => {
+                this.films = response.data;   
+            });
+        }
+    },
+    created() {
+        this.getFilms();
+    }
+}
+
+</script>
